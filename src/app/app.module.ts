0dead0159cbf2e35e55e6de5040ffae954e74621
Lakshmi Sharma <lakshmisharma92@gmail.com>
2018-08-29 10:59:40 -0500

    Component by CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Parent } from './parent/parent.component';
+import { ChildComponent } from './parent/child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Parent
+    Parent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
